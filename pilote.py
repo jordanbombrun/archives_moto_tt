972091bb425b748b506fd0af87b98394a68b1da9
bjordan <bjordan.dev@outlook.com>
2025-02-05 23:33:44 +0100

    ajout objets Pilote et Chrono

---
+++ pilote.py
@@ -0,0 +1,24 @@
+# pilote.py
+
+from chrono import Chrono
+
+
+class Pilote:
+    def __init__(self, position, numero, nom, tour):
+        self.position = position  # Position dans la course
+        self.numero = numero  # Numéro du pilote
+        self.nom = nom  # Nom du pilote
+        self.tour = tour  # Nombre de tours effectués
+        self.chronos = Chrono()
+
+    def __str__(self):
+        tempChronos = ""
+        for chr in self.chronos.temps_passage:
+            tempChronos += chr + " / "
+        return (
+            f"Position : {self.position}\n"
+            f"Numéro : {self.numero}\n"
+            f"Nom pilote : {self.nom}\n"
+            f"Tour: {self.tour}\n"
+            f"Chronos: {tempChronos}\n"
+        )
--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import os
 from bs4 import BeautifulSoup
+from chrono import Chrono
+from pilote import Pilote 
 
 # print(os.getcwd())
 filename = 'pilote1.html'
@@ -24,6 +26,18 @@ sec_row_td_values = [td.text.strip() for td in sec_row.find_all("td")]
 
 # 4 - extraction toutes les lignes
 all_rows = soup.find_all("tr")
+i = 0
 for row in all_rows:
     td_values = [td.text.strip() for td in row.find_all("td")]
-    print(td_values)
+    if (i == 0): 
+        table_header = Pilote(td_values[0], td_values[1], td_values[2], td_values[3])
+    elif (i == 1):
+        pilote1 = Pilote(td_values[0], td_values[1], td_values[2], td_values[3])
+        pilote1.chronos.ajouter_temps(td_values[4], td_values[5], td_values[6], td_values[7])
+    i += 1
+    # print(td_values)
+
+
+# classement au CP1
+print(pilote1)
+# print(pilote1)
