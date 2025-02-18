2298bb7cb87a5eea9fe50b63f67cc81bc8f38d8d
bjordan <bjordan.dev@outlook.com>
2025-02-18 22:18:45 +0100

    get_current_rank() fonctionne + gestion cas particuliers chronos

--- pilote.py
+++ pilote.py
@@ -1,5 +1,8 @@
 # pilote.py
 
+from datetime import time
+
+
 class Pilote:
     def __init__(self, position, numero, nom, tour):
         self.position = position  # Position dans la course
@@ -9,16 +12,36 @@ class Pilote:
         self.chronos = [] # liste des temps de passage
 
     def ajouter_chrono(self, *temps):
-        self.chronos.extend(temps)  # Ajoute tous les temps donnés à la liste
+        for t in temps:
+            chrono = self.convertir_en_time(t)
+            if chrono:  # Ajoute uniquement si la conversion a réussi
+                self.chronos.append(chrono)
+    
+    def convertir_en_time(self, horaire):
+        try:
+            if ":" not in horaire:
+                raise ValueError(f"Format invalide: {horaire}")
+            h, m = horaire.split(":")
+            
+            if not h.isdigit() or not m.isdigit():
+                raise ValueError(f"Valeurs invalides: {horaire}")
+            h, m = int(h), int(m)
 
-    def __str__(self):
-        tempChronos = ""
-        for chr in self.chronos.temps_passage:
-            tempChronos += chr + " / "
-        return (
-            f"Position : {self.position}\n"
-            f"Numéro : {self.numero}\n"
-            f"Nom pilote : {self.nom}\n"
-            f"Tour: {self.tour}\n"
-            f"Chronos: {tempChronos}\n"
-        )
+            if not (0 <= h < 24 and 0 <= m < 60):  # Vérification des limites
+                raise ValueError(f"Valeurs hors limites: {horaire}")
+            return time(h, m)
+        except ValueError as e:
+            # print(f"Erreur : {e}")  # Affichage de l'erreur
+            return time(0, 0)  # Retourne 0:0 en cas d'erreur
+
+    # def __str__(self):
+    #     tempChronos = ""
+    #     for chr in self.chronos.temps_passage:
+    #         tempChronos += chr + " / "
+    #     return (
+    #         f"Position : {self.position}\n"
+    #         f"Numéro : {self.numero}\n"
+    #         f"Nom pilote : {self.nom}\n"
+    #         f"Tour: {self.tour}\n"
+    #         f"Chronos: {tempChronos}\n"
+    #     )
--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import os
-from datetime import date
+from datetime import date, time
 from bs4 import BeautifulSoup
 from pilote import Pilote 
 from course import Course
@@ -7,8 +7,7 @@ from course import Course
 #############
 # Functions
 # extrait toutes les données pilotes et chronos depuis le tableau HTML
-def extract_datas(pilotes):
-    filename = 'pilote1.html'
+def extract_datas(filename):
     with open(filename, "r", encoding="utf-8") as file:
         html_content = file.read()
     soup = BeautifulSoup(html_content, "html.parser")
@@ -19,33 +18,43 @@ def extract_datas(pilotes):
         if (i == 0): 
             table_header = Pilote(td_values[0], td_values[1], td_values[2], td_values[3])
         else :
-            pilotes.append(Pilote(td_values[0], td_values[1], td_values[2], td_values[3]))
+            course.pilotes.append(Pilote(td_values[0], td_values[1], td_values[2], td_values[3]))
             for j, chrono_CP in enumerate(td_values):
                 if (j > 3):
-                    pilotes[i-1].ajouter_chrono(chrono_CP)
+                    course.pilotes[i-1].ajouter_chrono(chrono_CP)
         i += 1
 
 # Donne le classement d'un pilote pour un CP donné
 def get_current_rank(current_CP_P, pilote_P):
     current_position_L = 1
-    current_chrono_L = pilote_P.chronos[current_CP_P-1]
-    for k, pilote_L in enumerate(pilotes):
-        if pilote_L != pilote_P and current_chrono_L > pilote_L.chronos[current_CP_P-1]:
+    current_chrono_L = pilote_P.chronos[current_CP_P]
+    for k, pilote_L in enumerate(course.pilotes):
+        if current_chrono_L == time(0, 0):
+            current_position_L = 0
+            break
+        chrono_to_compare = pilote_L.chronos[current_CP_P]
+        if pilote_L != pilote_P and chrono_to_compare != time(0,0) and current_chrono_L > chrono_to_compare:
             current_position_L +=1
     return current_position_L
 
     
 #############
 # Main code
-pilotes = []
 course = Course("Alestrem", date(2025, 1, 26))
-extract_datas(pilotes)
-course.ajouter_pilotes(pilotes)
-course.nb_CP = len(pilotes[0].chronos)
+extract_datas("pilote1.html")
+course.nb_CP = len(course.pilotes[0].chronos)
 
+# pilote[2] : for each CP get_current_position > positions[]
+print("Young :")
+for current_CP in range(course.nb_CP):
+    print("CP " + str(current_CP+1) + " : " + str(get_current_rank(current_CP, course.pilotes[2])))
 
-# classement pilote 2 au CP2 - ne marche pas ?
-# print("Position du pilote " + pilotes[2].nom + " au CP 2 : " + str(get_current_rank(2, pilotes[2])) + " à l'heure : " + pilotes[2].chronos[1])
+print("Roman :")
+for current_CP in range(course.nb_CP):
+    print("CP " + str(current_CP+1) + " : " + str(get_current_rank(current_CP, course.pilotes[1])))
 
-print("nombre de pilotes : " + str(len(course.pilotes)))
-print("nombre de CP : " + str(course.nb_CP))
+print("Kabach :")
+for current_CP in range(course.nb_CP):
+    print("CP " + str(current_CP+1) + " : " + str(get_current_rank(current_CP, course.pilotes[0])))
+
+# print("Position du pilote " + course.pilotes[2].nom + " au CP 2 : " + str(get_current_rank(2, course.pilotes[2])) + " à l'heure : " + str(course.pilotes[2].chronos[1]))
