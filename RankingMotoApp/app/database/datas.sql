cb2ce33f856920c5d82dae44d72d1a66a063b8e0
bjordan <bjordan.dev@outlook.com>
2025-08-11 21:53:12 +0200

    update infos init BDD

--- RankingMotoApp/app/database/datas.sql
+++ RankingMotoApp/app/database/datas.sql
@@ -1,9 +1,9 @@
 -- schema of the DB in schema.sql
 INSERT INTO category (name)
-VALUES ('black'), ('red'), ('green'), ('blue'), ('pink'), ('or'), ('argent'), ('bronze'),('elite'), ('junior'), ('espoir'), ('veteran'), ('super veteran'), ('feminin'), ('national'), ('regional');
+VALUES ('black'), ('red'), ('green'), ('blue'), ('pink'), ('or'), ('argent'), ('bronze'),('elite'), ('junior'), ('espoir'), ('veteran'), ('super veteran'), ('feminin'), ('open'), ('nationale'), ('nationale E1'), ('nationale E2'), ('nationale E3'), ('regionale'), ('regionale E1'), ('regionale E2'), ('regionale E3');
 
 INSERT INTO serie (name, year)
 VALUES ('Extrême challenge', 2025), ('Ligue Aura enduro', 2025), ('Championnat de France enduro', 2025), ('Championnat de France motocross', 2025), ('Championnat de France Cross Country', 2025); 
 
 INSERT INTO format (name)
-VALUES ('Enduro'), ('Cross country'), ('Enduro extrême'), ('Motocross'), ('Supercross'), ('Enduro sprint'), ('Rallye'), ('Super enduro');
+VALUES ('Enduro'), ('Cross country'), ('Endurance'), ('Enduro extrême'), ('Motocross'), ('Supercross'), ('Enduro sprint'), ('Rallye'), ('Super enduro');
--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
-## Liste des commandes bash pour installation sur linux 
+## Pour installation sur linux 
 
+### sources et environnement
 git clone https://github.com/jordanbombrun/archives_moto_tt.git
 
 git checkout front_flask
@@ -9,3 +10,6 @@ cd archives_moto_tt
 chmod +x setup.sh 
 
 ./setup.sh
+
+### BDD : init schema et data
+app/database/init_db.py
